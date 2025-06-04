c9f678c3386dff1ec6505534052d588118520873
nseon <nseon@student.42lyon.fr>
2025-06-04 12:59:36 +0200

    fix: color_mix & refactor others color op

--- src/utils/colors.c
+++ src/utils/colors.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   colors.c                                           :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: pjarnac <pjarnac@student.42lyon.fr>        +#+  +:+       +#+        */
+/*   By: nseon <nseon@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/07 10:52:46 by pjarnac           #+#    #+#             */
-/*   Updated: 2025/01/07 10:52:46 by pjarnac          ###   ########.fr       */
+/*   Updated: 2025/06/04 12:58:12 by nseon            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,24 +20,24 @@ t_color	argb(uint8_t a, uint8_t r, uint8_t g, uint8_t b)
 uint32_t	colorx(int32_t color, float m)
 {
 	t_color		ncolor;
-	uint32_t	rgb[3];
+	uint32_t	rgb;
 
 	ncolor.argb = color;
-	rgb[0] = ncolor.r * m;
-	if (rgb[0] > 255)
+	rgb = ncolor.r * m;
+	if (rgb > 255)
 		ncolor.r = 255;
 	else
-		ncolor.r = rgb[0];
-	rgb[1] = ncolor.g * m;
-	if (rgb[1] > 255)
+		ncolor.r = rgb;
+	rgb = ncolor.g * m;
+	if (rgb > 255)
 		ncolor.g = 255;
 	else
-		ncolor.g = rgb[1];
-	rgb[1] = ncolor.b * m;
-	if (rgb[1] > 255)
+		ncolor.g = rgb;
+	rgb = ncolor.b * m;
+	if (rgb > 255)
 		ncolor.b = 255;
 	else
-		ncolor.b = rgb[1];
+		ncolor.b = rgb;
 	return (ncolor.argb);
 }
 
@@ -45,34 +45,46 @@ uint32_t	colorp(uint32_t color, uint32_t color2)
 {
 	t_color		ncolor;
 	t_color		ncolor2;
-	uint32_t	rgb[3];
+	uint32_t	rgb;
 
 	ncolor.argb = color;
 	ncolor2.argb = color2;
-	rgb[0] = ncolor.r + ncolor2.r;
-	if (rgb[0] > 255)
+	rgb = ncolor.r + ncolor2.r;
+	if (rgb > 255)
 		ncolor.r = 255;
 	else
-		ncolor.r = rgb[0];
-	rgb[1] = ncolor.g + ncolor2.g;
-	if (rgb[1] > 255)
+		ncolor.r = rgb;
+	rgb = ncolor.g + ncolor2.g;
+	if (rgb > 255)
 		ncolor.g = 255;
 	else
-		ncolor.g = rgb[1];
-	rgb[1] = ncolor.b + ncolor2.b;
-	if (rgb[1] > 255)
+		ncolor.g = rgb;
+	rgb = ncolor.b + ncolor2.b;
+	if (rgb > 255)
 		ncolor.b = 255;
 	else
-		ncolor.b = rgb[1];
+		ncolor.b = rgb;
 	return (ncolor.argb);
 }
 
-uint32_t	color_mix(uint32_t color1, uint32_t color2)
+uint32_t		color_mix(uint32_t actual_color, uint32_t mix_color, uint32_t nb_mix)
 {
-	uint32_t	col1;
-	uint32_t	col2;
+	t_color 	old;
+	t_color		mix;
+	uint32_t	r;
+	uint32_t	g;
+	uint32_t	b;
 
-	col1 = colorx(color1, 0.5);
-	col2 = colorx(color2, 0.5);
-	return (colorp(col1, col2));
+	old.argb = actual_color;
+	mix.argb = mix_color;
+	r = (nb_mix * old.r + mix.r) * ((float)1 / (nb_mix + 1));
+	if (r > 255)
+		r = 255;
+	g = (nb_mix * old.g + mix.g) * ((float)1 / (nb_mix + 1));
+	if (g > 255)
+		g = 255;
+	b = (nb_mix * old.b + mix.b) * ((float)1 / (nb_mix + 1));
+	if (b > 255)
+		b = 255;
+	return (r * 0x10000 + g * 0x100 + b);
 }
